b8f337193d033d20c2ff61e378346006bf594f50
Fabrizio Platero <fabriplatero88@gmail.com>
2024-01-23 11:23:18 -0300

    added ProductManager but now with async methods using fs promises

---
+++ src/ProductManager.js
@@ -0,0 +1,70 @@
+import fs from 'fs/promises';
+
+class ProductManager {
+  constructor() {
+    this.path = "./products.json";
+    this.loadProducts();
+  }
+
+  async loadProducts() {
+    try {
+      const data = await fs.readFile(this.path, "utf8");
+      this.products = JSON.parse(data);
+    } catch (error) {
+      this.products = [];
+    }
+  }
+
+  async saveProducts() {
+    const data = JSON.stringify(this.products, null, 2);
+    await fs.writeFile(this.path, data);
+  }
+
+  async addProduct(product) {
+    product.id = this.products.length + 1;
+    this.products.push(product);
+    await this.saveProducts();
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  async getProductById(id) {
+    try {
+      const foundProduct = this.products.find((product) => product.id === id);
+
+      if (foundProduct) {
+        return foundProduct;
+      } else {
+        throw new Error("Producto no encontrado");
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateProduct(id, upFields) {
+    const i = this.products.findIndex((product) => product.id === id);
+    if (i !== -1) {
+      this.products[i] = { ...this.products[i], ...upFields };
+      await this.saveProducts();
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      const i = this.products.findIndex((product) => product.id === id);
+      if (i !== -1) {
+        this.products.splice(i, 1);
+        await this.saveProducts();
+      } else {
+        throw new Error("Producto no encontrado");
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default ProductManager;
