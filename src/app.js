8f3f81b1ffad7a54c69b5f77482eab2493780f7d
Fabrizio Platero <fabriplatero88@gmail.com>
2024-01-23 12:33:38 -0300

    added products endpoint using query params with its validations

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from 'express'
+import ProductManager from './ProductManager.js';
 
 const app = express();
+const productManager = new ProductManager();
 
 app.use(express.urlencoded({extended:true}))
 
 app.set("PORT", 8080)
+
+app.get("/products", async (req, res)=>{
+    await productManager.loadProducts();
+
+    const limit = req.query.limit;
+    let products = productManager.getProducts();
+  
+    if (limit) {
+      products = products.slice(0, parseInt(limit, 10));
+    }  
+
+    res.json({ products });
+    
+})
 
 export default app
